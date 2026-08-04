c23005cc5aded2bf69fc11811d2f81fa01e3dca1
daraf <dara@pop-os.fritz.box>
2026-08-05 01:08:25 +0200

    Safe

--- src/version.hpp
+++ src/version.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
 #define BUILD_VERSION "v0.3.3"
-#define BUILD_COMMIT "b641083e58fed295ba0a182ad077c6b85650f105"
-#define BUILD_DATE "2026-08-02"
+#define BUILD_COMMIT "44f694d5672be5743b4535a5ab8f384d16cb0ed6"
+#define BUILD_DATE "2026-08-04"
